1fd022cb575f84a0a94cfc4448c155b8589bd70f
John Backus <johncbackus@gmail.com>
2022-03-19 21:55:46 -0400

    Add resolve ignore list

--- ens.go
+++ ens.go
@@ -1,13 +1,71 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"os"
+
 	"github.com/ethereum/go-ethereum/ethclient"
 	ens "github.com/wealdtech/go-ens/v3"
 )
 
+type IgnoreList struct {
+	path string
+	list []ENSDomain
+}
+
+func LoadIgnoreList(path string) []ENSDomain {
+	result, err := os.ReadFile(path)
+	check(err)
+
+	data := []ENSDomain{}
+	json.Unmarshal(result, &data)
+
+	return data
+}
+
+func NewIgnoreList(path string) IgnoreList {
+	exists, err := fileExists(path)
+	check(err)
+
+	data := []ENSDomain{}
+
+	if exists {
+		data = LoadIgnoreList(path)
+	} else {
+		serialized, err := json.Marshal(data)
+		check(err)
+
+		os.WriteFile(path, serialized, 0777)
+	}
+
+	return IgnoreList{path, data}
+}
+
+func (list IgnoreList) Has(domain ENSDomain) bool {
+	for _, item := range list.list {
+		if item == domain {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (list IgnoreList) Add(domain ENSDomain) {
+	currentList := LoadIgnoreList(list.path)
+	newList := append(currentList, domain)
+
+	serialized, err := json.Marshal(newList)
+	check(err)
+
+	os.WriteFile(list.path, serialized, 0777)
+}
+
 type ENSClient struct {
-	client *ethclient.Client
-	cache  FileSystemCache
+	client     *ethclient.Client
+	cache      FileSystemCache
+	ignoreList IgnoreList
 }
 
 func NewENSClient(infuraUrl string) ENSClient {
@@ -16,7 +74,9 @@ func NewENSClient(infuraUrl string) ENSClient {
 
 	cache := NewFileSystemCache("data/ens")
 
-	return ENSClient{client, cache}
+	ignoreList := NewIgnoreList("data/ens/ignore.json")
+
+	return ENSClient{client, cache, ignoreList}
 }
 
 func (domain ENSDomain) CacheKey() string {
@@ -24,6 +84,9 @@ func (domain ENSDomain) CacheKey() string {
 }
 
 func (client ENSClient) CachedResolve(domain ENSDomain) (string, error) {
+	if client.ignoreList.Has(domain) {
+		return "", errors.New("domain failed to resolve and has been marked as ignored")
+	}
 	var address string
 	if client.cache.IsCached(domain) {
 		address = string(client.cache.ReadCache(domain))
@@ -31,6 +94,7 @@ func (client ENSClient) CachedResolve(domain ENSDomain) (string, error) {
 		result, err := client.Resolve(domain)
 
 		if err != nil {
+			client.ignoreList.Add(domain)
 			return "", err
 		}
 
